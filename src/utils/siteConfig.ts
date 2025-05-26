7b90b42b5412f338dd6d773438d115a28bf33440
Confidence Ugolo <ugoloconfidence@gmail.com>
2025-05-26 22:28:57 +0100

    New blog content

--- src/utils/siteConfig.ts
+++ src/utils/siteConfig.ts
@@ -78,8 +78,10 @@ interface SiteConfig {
 
 import image1 from '@/assets/images/101589e1a6decdc226f18a128678c504.jpg'
 import image2 from '@/assets/images/1c2f243cdf51a73f0c1d326159aaa492.jpg'
+import image3 from '@/assets/images/546fdgfre454.png'
 import content1 from '@/assets/blogs/article-1.md'
 import content2 from '@/assets/blogs/article-2.md'
+import content3 from '@/assets/blogs/article-3.md'
 
 export const siteConfig: SiteConfig = {
     socialLinks: {
@@ -158,6 +160,16 @@ export const siteConfig: SiteConfig = {
             date: '2024-12-02',
             images: [image2],
             content: content2,
+            tags: ['nutrition', 'health', 'wellness'],
+            featured: true
+        }),
+        addSlugToBlogPost({
+            id: '3',
+            title: 'Fighting for Your Healthspan After 55: The New Era of Personalized Longevity',
+            description: 'Introduction: A New Chapter in Aging',
+            date: '2024-12-02',
+            images: [image3],
+            content: content3,
             tags: ['nutrition', 'health', 'wellness'],
             featured: true
         }),
